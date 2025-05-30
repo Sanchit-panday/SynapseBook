a9903a2adfa66a632954436ae1cabcacd17335fc
Sanchit Pandey <hydhexane@gmail.com>
2025-05-30 19:13:29 +0530

    [Fixed] card content overflow

--- Models/Notes.cs
+++ Models/Notes.cs
@@ -4,7 +4,7 @@
     {
         public required int Id {  get; set; }
         public required string ReferenceNo { get; set; }
-        public required string Title { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
     }
 }
